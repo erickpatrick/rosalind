7d646b1f4a2ec64905d5f04763672a343298ffb4
Erick Patrick <programming@erickpatrick.com>
2014-08-05 13:44:29 -0300

    Refactored 5th challenge
    http://rosalind.info/problems/gc/

--- solutions/0005-computing-gc-content/solution.php
+++ solutions/0005-computing-gc-content/solution.php
@@ -1,19 +1,11 @@
 <?php
 
 	function gcContent($input) {
-		$inputCounter = strlen($input);
-		$counter = 0;
+		$input = str_split($input);
+		$counter = array_count_values($input);
+		$inputCounter = count($input);
 
-		for ($i = 0; $i < $inputCounter; $i += 1) {
-			switch ($input[$i]) {
-				case 'G' : $counter += 1; break;
-				case 'C' : $counter += 1; break;
-			}
-		}
-
-		// echo $input . " >> " . $inputCounter . " >> " . $counter . "<br/>"; 
-
-		return (($counter / $inputCounter) * 100);
+		return ((($counter['G'] + $counter['C']) / $inputCounter) * 100);
 	}
 
 	function computingGCContent($input)
@@ -21,30 +13,24 @@
 		$dataset = [];
 		$input = explode(">", $input);
 		foreach ($input as $i) {
-			$set = explode(" ", $i);
-			$dataset[] = [
-				"name" => $set[0], 
-				"sequence" => $set[1], 
-				"gc_content" => null
-			];
+			$dataset[] = explode(" ", $i);
 		}
 
 		unset($dataset[0]);
 		$datasetCounter = count($dataset);
 
 		for ($i = 1; $i <= $datasetCounter; $i += 1) {
-			$dataset[$i]['gc_content'] = gcContent($dataset[$i]['sequence']);
+			$dataset[$i][] = gcContent($dataset[$i]['1']);
 		}
 
 		return $dataset;
 	}
 
-	$input = "
-	>Rosalind_6404 CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG
+	$input = ">Rosalind_6404 CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG
 	>Rosalind_5959 CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC>Rosalind_0808 CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT";
 	$dataset = computingGCContent($input);
 
 	foreach ($dataset as $d) {
-		echo "<p>{$d['name']}<br/>{$d['gc_content']}";
+		echo "<p>{$d['0']}<br/>{$d['2']}";
 	}
 ?>
