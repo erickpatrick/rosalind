b8188f705b2ad00b01c0fddcf594a94e41a80503
Erick Patrick <programming@erickpatrick.com>
2014-08-05 13:33:36 -0300

    Refactored 3rd challenge

--- solutions/0003-complementing-strand-dna/solution.php
+++ solutions/0003-complementing-strand-dna/solution.php
@@ -1,19 +1,7 @@
 <?php 
 	function complementingStrandDNA($input)
 	{
-		$input = strrev($input);
-		$inputLength = strlen($input);
-
-		for ($i = 0; $i < $inputLength; $i += 1) {
-			switch ($input[$i]) {
-				case 'A' : $input[$i] = 'T'; break;
-				case 'C' : $input[$i] = 'G'; break;
-				case 'G' : $input[$i] = 'C'; break;
-				case 'T' : $input[$i] = 'A'; break;
-			}
-		}
-
-		return $input;
+		return strtr(strrev($input), ["A" => "T", "C" => "G", "G" => "C", "T" => "A"]);
 	}
 
 	$input = "AAAACCCGGT";
