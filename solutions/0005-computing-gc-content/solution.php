7982d4ba3b008d3f5157e5979708291a2e15e5c1
Erick Patrick <programming@erickpatrick.com>
2014-08-05 13:12:16 -0300

    Fifth challenge. Needs tests

---
+++ solutions/0005-computing-gc-content/solution.php
@@ -0,0 +1,13 @@
+<?php 
+	function rabbitsRecurrenceRelations($n, $k)
+	{
+		$months = [1, 1];
+		for ($i = 2; $i < $n; $i += 1) {
+			$months[$i] = $months[$i - 1] + ($months[$i - 2] * $k);
+		}
+
+		return $months[$n - 1];
+	}
+
+	echo rabbitsRecurrenceRelations(5, 3);
+?>
---
+++ solutions/0005-computing-gc-content/test.php
@@ -0,0 +1,5 @@
+<?php 
+	/**
+	 * @TODO - Do test the function
+	 */
+?>
