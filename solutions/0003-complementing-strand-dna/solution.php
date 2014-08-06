c419e775737f25a4a143ddaa65372a0056fec8d2
Erick Patrick <programming@erickpatrick.com>
2014-08-06 08:33:39 -0300

    REmoved old 3rd challenge files

--- solutions/0003-complementing-strand-dna/solution.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	function complementingStrandDNA($input)
-	{
-		return strtr(strrev($input), ["A" => "T", "C" => "G", "G" => "C", "T" => "A"]);
-	}
-
-	$input = "AAAACCCGGT";
-	echo complementingStrandDNA($input);
-?>
