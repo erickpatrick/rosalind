587a83c718b18c60365df8e8ce294428350b5e0e
Erick Patrick <programming@erickpatrick.com>
2014-09-09 17:36:27 -0300

    raising errors instead of exiting app
    raising error is better to understand why an app stoped

--- solutions/ruby/rosalind/00006.hamm/src/app.rb
+++ solutions/ruby/rosalind/00006.hamm/src/app.rb
@@ -1,8 +1,8 @@
 dna1 = 'GAGCCTACTAACGGGAT'
 dna2 = 'CATCGTAATGACGGCCT'
 
-Process.exit! "dna1 and dn2 lengths don't match" unless dna1.length == dna2.length
-Process.exit! "dna strings bigger than 1kbp" unless dna1.length % 2 < 1000
+raise "dna1 and dn2 lengths don't match" unless dna1.length == dna2.length
+raise "dna strings bigger than 1kbp" unless dna1.length / 2 < 1000
 
 counter = 0
 (0..dna2.length).each do |i|
