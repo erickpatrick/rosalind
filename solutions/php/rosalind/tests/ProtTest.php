cc6683ece25037a1cfc56ad2a99f0ec01f0e9bc1
Erick Patrick <github@erickpatrick.net>
2015-11-21 22:04:54 +0100

    Changing folder to a 'better structure':

---
+++ solutions/php/rosalind/tests/ProtTest.php
@@ -0,0 +1,31 @@
+<?php require __DIR__ . '/vendor/autoload.php';
+
+use Acme\Prot;
+
+class ProtTest extends PHPUnit_Framework_TestCase {
+	/** @var Acme\Prot $prot */
+	private $prot;
+
+	public function setUp()
+	{
+		$this->prot = new Prot();
+	}
+
+	/**
+	* @dataProvider dataProviderSampleInput
+	*/
+	public function testTranslateRnaIntoProteinWithSampleInput($sampleRna, $sampleResult)
+	{
+		$this->assertEquals(
+			$this->prot->translateRnaIntoProtein($sampleRna), 
+			$sampleResult
+		);
+	}
+
+	public function dataProviderSampleInput()
+	{
+		return [
+			['AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA', 'MAMAPRTEINSTRING']
+		];
+	}
+}
