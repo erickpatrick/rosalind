1867feb1ff4d2cd7645d6ee46409d636f23702d8
Erick Patrick <github@erickpatrick.net>
2015-11-22 00:08:47 +0100

    Solved: PROT - Translating RNA into Protein

--- solutions/php/rosalind/src/Prot.php
+++ solutions/php/rosalind/src/Prot.php
@@ -1,7 +1,7 @@
 <?php namespace Acme;
 
 class Prot {
-    private $codonTable = [
+    private $rnaCodonsTable = [
         'F' => ['UUU', 'UUC'],
         'L' => ['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],
         'S' => ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],
@@ -21,26 +21,81 @@ class Prot {
         'A' => ['GCU', 'GCC', 'GCA', 'GCG'],
         'D' => ['GAU', 'GAC'],
         'E' => ['GAA', 'GAG'],
-        'G' => ['GGU', 'GGC', 'GGA', 'GGG']
+        'G' => ['GGU', 'GGC', 'GGA', 'GGG'],
     ];
-    
+
     private $stopTable = ['UAA', 'UAG', 'UGA'];
 
+    /**
+     * Translates given RNA string into a propert protein
+     * 
+     * @param string $rnaString
+     * @return string
+     * @throws InvalidArgumentException
+     */
     public function translateRnaIntoProtein($rnaString)
     {
-        $this->isValidmRna($rnaString);
+        $protein = [];
 
-        $codons = str_split($rnaString, 3);
-        return count($codons);
-        //return 'MAMAPRTEINSTRING';
+        if ($this->isValidmRna($rnaString)) {
+            $rnaCodons = str_split($rnaString, 3);
+
+            foreach ($rnaCodons as $rnaCodon) {
+                if ($this->isStopCondition($rnaCodon)) {
+                    break;
+                }
+
+                $protein[] = $this->getAminoacid($rnaCodon);
+            }
+        } else {
+            throw new \InvalidArgumentException('Invalid argument');
+        }
+
+        return implode("", $protein);
     }
 
+    /**
+     * Verifies if given strand of RNA is a valid messenger RNA by checking its size
+     * 
+     * @param string $rnaString
+     * @return bool
+     */
     private function isValidmRna($rnaString)
     {
         $numberOfNucleotides = strlen($rnaString);
 
-        if ($numberOfNucleotides / 2 > 10000) {
-            throw new \InvalidArgumentException('Invalid argument');
-        }
+        return ($numberOfNucleotides / 2 < 10000);
+    }
+
+    /**
+     * Verifies if given codon is a stop condition to terminate RNA translation into protein
+     * 
+     * @param string $rnaCodon
+     * @return bool
+     */
+    private function isStopCondition($rnaCodon)
+    {
+        return in_array($rnaCodon, $this->stopTable);
+    }
+
+    /**
+     * Searches through codons table and returns proper aminoacid for given codon
+     * 
+     * @param string $rnaString
+     * @return string
+     */
+    private function getAminoacid($rnaCodon)
+    {
+        $selectedAminoacidTable = array_filter(
+            $this->rnaCodonsTable, 
+            function($rnaCodons, $aminoacid) use ($rnaCodon) {
+                return in_array($rnaCodon, $rnaCodons);
+            }, 
+            ARRAY_FILTER_USE_BOTH
+        );
+
+        $aminoacid = array_keys($selectedAminoacidTable)[0];
+
+        return $aminoacid;
     }
 }
