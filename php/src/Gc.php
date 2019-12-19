9162845ee507ffae5a46215f3221f4307f038f3b
Erick Patrick <github@erickpatrick.net>
2019-12-19 12:55:13 +0100

    fixes code to work with rosalind dataset

--- php/src/Gc.php
+++ php/src/Gc.php
@@ -1,37 +1,77 @@
-<?php namespace Acme;
+<?php
 
-class Gc 
+namespace Acme;
+
+class Gc
 {
-    public function gcContent($input) {
-        $input = str_split($input);
-        $counter = array_count_values($input);
-        $inputCounter = count($input);
-
-        return ((($counter['G'] + $counter['C']) / $inputCounter) * 100);
-    }
-
-    public function properDataset($input) {
-        $dataset = [];
-
-        $input = explode(">", $input);
-        foreach ($input as $i) {
-            $dataset[] = explode(" ", $i);
-        }
-        unset($dataset[0]);
-
-        return $dataset;
-    }
-
-    public function computingGCContent($input)
+    public function computingGcContent(string $input): string
     {
-        $dataset = $this->properDataset($input);
+        $labels = $this->getLabels($input);
+        $dnas = $this->getDnas($input);
+        $results = $this->calculateGcContent($dnas);
 
-        $datasetCounter = count($dataset);
+        return $this->getHighestGcContent($labels, $results);
+    }
 
-        for ($i = 1; $i <= $datasetCounter; $i += 1) {
-            $dataset[$i][] = $this->gcContent($dataset[$i]['1']);
+    private function getLabels(string $input): array
+    {
+        // array_values necessary in order to reset keys to associate
+        // them to DNA strings
+        return array_values(array_filter(array_map(
+            // removes spaces from both ends and remove `>` DNA String name
+            fn ($label) => trim(str_replace('>', '', $label)),
+
+            // find DNA String name and remove empty entries
+            preg_split('/([CGTA]+)/', $input, -1, PREG_SPLIT_NO_EMPTY)
+        )));
+    }
+
+    private function getDnas(string $input): array
+    {
+
+        return array_filter(array_map(
+            // removes spaces from both ends and merge lines for each string
+            fn ($dna) => trim(preg_replace('/\n/', '', $dna)),
+
+            // slipt where each dns string starts and remove empty entries
+            preg_split('/(>Rosalind_[0-9]+)/', $input, -1, PREG_SPLIT_NO_EMPTY)
+        ));
+    }
+
+    private function calculateGcContent(array $dnas): array
+    {
+        $results = [];
+        
+        foreach ($dnas as $dna) {
+            $allSymbols = str_split($dna);
+            $countBySymbols = array_count_values($allSymbols);
+            $symbolsCounter = count($allSymbols);
+
+            $totalForGandC = $countBySymbols['G'] + $countBySymbols['C'];
+            $percentage = (($totalForGandC / $symbolsCounter) * 100);
+
+            // sets max precision to 6 numbers
+            $results[] = number_format($percentage, 6);
         }
 
-        return $dataset;
+        // order by highest value keeping keys associated
+        arsort($results);
+
+        return $results;
+    }
+
+    private function getHighestGcContent(array $labels, array $results): string
+    {
+        $resultLabel = '';
+
+        // O(1) as it is ordered and we get out of the loop just after the 1st
+        // item. We do this as we need to get the key as well in order to grab
+        // the label to display in the result
+        foreach ($results as $key => $result) {
+            $resultLabel = $labels[$key] . PHP_EOL . $result;
+            break;
+        }
+
+        return $resultLabel;
     }
 }
